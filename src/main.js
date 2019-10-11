ffc493a9067f681296add4ed3bfd502e74db7f9e
Stephen Richardson <srichardson@reactual.io>
2019-10-10 19:17:34 -0600

    Misc formatting

--- src/main.js
+++ src/main.js
@@ -3,15 +3,14 @@ const x = require('x-ray')()
 module.exports = async (id = '', limit = 1, offset = 1, context) => {
   if (!id || typeof id !== 'string') throw Error('invalid user id')
 
-  const user = id.toLowerCase()
-  const page = offset < 1 ? 1 : offset
-  const url = `https://news.ycombinator.com/favorites?id=${user}&p=${page}`
-
-  const selector = 'tr.athing'
-  const articleId = '@id'
-  const title = 'a.storylink'
-  const link = `${title}@href`
-  const more = 'a.morelink@href'
+  const user = id.toLowerCase(),
+    articleId = '@id',
+    title = 'a.storylink',
+    link = `${title}@href`,
+    selector = 'tr.athing',
+    more = 'a.morelink@href',
+    page = offset < 1 ? 1 : offset,
+    url = `https://news.ycombinator.com/favorites?id=${user}&p=${page}`
 
   return await x(url, selector, [{ id: articleId, title, link }])
     .paginate(more)
