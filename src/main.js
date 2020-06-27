e19a71bb1453190197fe62fbcaa38891210a8053
Stephen Richardson <srichardson@reactual.io>
2020-06-27 03:45:16 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const BASE_URL = 'https://news.ycombinator.com/favorites',
 
 module.exports = async (id, limit, offset, context) => {
   if (typeof id !== 'string' || !(id.length > 0)) {
-    throw TypeError('The user id argument is a required, non-empty string.')
+    throw new TypeError('The user id argument is a required, non-empty string.')
   }
 
   if (typeof limit !== 'number' || !(limit >= 1)) limit = 1
