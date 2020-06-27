2f1fdb3a04f526b6887c1d0933b48285c60331fd
Stephen Richardson <srichardson@reactual.io>
2020-06-27 03:43:50 -0600

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 const x = require('x-ray')()
 
-const MORE = 'a.morelink@href',
+const BASE_URL = 'https://news.ycombinator.com/favorites',
+  MORE = 'a.morelink@href',
   SELECTOR = 'tr.athing',
-  SELECTOR_LIST = [{ id: '@id', title: 'a.storylink', link: 'a.storylink@href' }],
-  BASE_URL = 'https://news.ycombinator.com/favorites'
+  SELECTOR_LIST = [
+    { id: '@id', title: 'a.storylink', link: 'a.storylink@href' },
+  ]
 
 module.exports = async (id, limit, offset, context) => {
   if (typeof id !== 'string' || !(id.length > 0)) {
     throw TypeError('The user id argument is a required, non-empty string.')
   }
-  
+
   if (typeof limit !== 'number' || !(limit >= 1)) limit = 1
   if (typeof offset !== 'number' || !(offset >= 1)) offset = 1
 
-  const userId = id.toLowerCase()
-  
-  return await x(`${BASE_URL}?id=${userId}&p=${offset}`, SELECTOR, SELECTOR_LIST)
+  const userId = id.toLowerCase(),
+    url = BASE_URL + `?id=${userId}&p=${offset}`
+
+  return await x(url, SELECTOR, SELECTOR_LIST)
     .paginate(MORE)
     .limit(limit)
 }
