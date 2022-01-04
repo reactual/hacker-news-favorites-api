309d1864ba82c67f6b75ec07cd4c289b0b9216ff
sbrichardson <srichardson@reactual.io>
2022-01-03 23:06:07 -0700

    fix renamed class titlelink, bump version

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
 const x = require('x-ray')()
 
-const BASE_URL = 'https://news.ycombinator.com/favorites',
-  MORE = 'a.morelink@href',
-  SELECTOR = 'tr.athing',
-  SELECTOR_LIST = [
-    {
-      id: '@id',
-      title: 'a.storylink',
-      link: 'a.storylink@href',
-    },
-  ]
+const BASE_URL = 'https://news.ycombinator.com/favorites'
+const MORE = 'a.morelink@href'
+const SELECTOR = 'tr.athing'
+const SELECTOR_LIST = [
+  {
+    id: '@id',
+    title: 'a.titlelink',
+    link: 'a.titlelink@href'
+  }
+]
 
 module.exports = async (id, limit, offset, context) => {
   if (!(typeof id === 'string' && id.length > 0)) {
     throw new TypeError('The user id argument is a required, non-empty string.')
   }
-  
+
   if (!(typeof limit === 'number' && limit >= 1)) limit = 1
   if (!(typeof offset === 'number' && offset >= 1)) offset = 1
 
-  const userId = id.toLowerCase(),
-    url = BASE_URL + `?id=${userId}&p=${offset}`
+  const userId = id.toLowerCase()
+  const url = BASE_URL + `?id=${userId}&p=${offset}`
 
   return await x(url, SELECTOR, SELECTOR_LIST)
     .paginate(MORE)
